08cddd65455b2a18b9775f8a5b5c53981ec33b35
Rob Olson <rolson@waisman.wisc.edu>
2011-02-16 13:40:08 -0600

    Add wisp.m as a wrapper around Experiment.m

---
+++ wisp.m
@@ -0,0 +1,11 @@
+function wisp()
+% WISP - Wisconsin Infant Stimulus Program
+%     or Wisconsin Infant Studies Program
+%     or Wisconsin Infant Stimulus Presentation
+%
+% Robert H. Olson, Ph.D., rolson@waisman.wisc.edu
+
+% Call Experiment.m
+Experiment
+
+end
