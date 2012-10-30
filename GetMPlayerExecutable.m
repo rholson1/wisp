6911e389b5e0fe6ab3e87341552d69d39f89eac7
Rob Olson <rolson@waisman.wisc.edu>
2012-10-30 15:20:49 -0500

    Use path to MPlayer included in SMPlayer distribution.

--- GetMPlayerExecutable.m
+++ GetMPlayerExecutable.m
@@ -6,8 +6,6 @@ function mplayerpath = GetMPlayerExecutable()
 
 % 2010-11-29 : Created by Robert H. Olson, Ph.D., rolson@waisman.wisc.edu
 
-
-mplayerpath = 'C:\Programs\MPlayer\mplayer.exe'; % IKARA
-%mplayerpath = 'C:\toolbox\Experiment\samples\MPlayer\mplayer.exe'; % NALA
+mplayerpath = 'C:\Program Files\SMPlayer\mplayer\mplayer.exe'; % IKARA
 
 end
