d55d7f52d4cf698ed411420fb498976d103c494a
Rob Olson <rholson1@wisc.edu>
2017-03-16 08:42:39 -0500

    Remove deprecated bufsize argument to textscan

--- gencode_rvalue.m
+++ gencode_rvalue.m
@@ -113,8 +113,7 @@ switch class(item)
                 else
                     sitem = mat2str(item,'class');
                 end
-                bsz   = max(numel(sitem)+2,100); % bsz needs to be > 100 and larger than string length
-                str1 = textscan(sitem, '%s', 'delimiter',';', 'bufsize',bsz); 
+                str1 = textscan(sitem, '%s', 'delimiter',';'); 
                 if numel(str1{1}) > 1
                     str = str1{1};
                 else
