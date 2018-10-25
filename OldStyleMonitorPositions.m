c45e616670328fe3b78c7b1d7600eb10083f060f
Rob Olson <rholson1@wisc.edu>
2018-10-25 11:32:22 -0500

    Allow OldStyleMonitorPositions to return unsorted monitor coordinates to fix bug in displaying images fullscreen in PlayImage.

--- OldStyleMonitorPositions.m
+++ OldStyleMonitorPositions.m
@@ -1,4 +1,4 @@
-function mp = OldStyleMonitorPositions()
+function mp = OldStyleMonitorPositions(unsorted)
 % OldStyleMonitorPositions - Return monitor positions in pre-2014b format
 
 mp = get(0, 'MonitorPositions');
@@ -8,7 +8,13 @@ if verLessThan('matlab', '8.4.0')
 else
    % R2014b or later
    % Convert to format [x1 y1 width height]
-   mp = sortrows(mp);
+   [mp, idx] = sortrows(mp);
    XY = [mp(:,1) -1*mp(:,2)-(mp(:,4)-mp(1,4))+2];
    mp = [XY XY+mp(:,3:4)-1];
+   if nargin == 1
+       % Reverse sort so that playimage works as expected for fullscreen
+       u = 1:size(mp, 1);
+       newidx(idx) = u;
+       mp = mp(newidx, :);
+   end
 end
